e0dabb573145a5725315b43a1839148fc9536075
Kazure Zheng <kazure@debian>
2026-02-20 22:11:49 +0800

    modify gen_projection

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -41,7 +41,8 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     t2 << zNear, 0, 0, 0, 0, zNear, 0, 0, 0, 0, zNear + zFar, -1 * zNear * zFar,
         0, 0, 1, 0; // perspective to orthographic
     // this matrix did not require move transformation
-    float height = zNear * std::tan(eye_fov) * 2;
+    float rad = eye_fov * std::acos(-1) / 180.0f;
+    float height = zNear * std::tan(rad) * 2;
     float width = height / aspect_ratio;
     float depth = zNear - zFar;
     t1 << 2 / width, 0, 0, 0, 0, 2 / height, 0, 0, 0, 0, 2 / depth, 0, 0, 0, 0,
