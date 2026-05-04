55b97b3cc75b27f838967aeb68faec96304ea243
Kazure Zheng <kazure@debian>
2026-05-04 22:10:31 +0800

    finish part 1

--- pa8/code/src/rope.cpp
+++ pa8/code/src/rope.cpp
@@ -11,51 +11,64 @@ namespace CGL {
 
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
-        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        // DONE (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        Vector2D step = (end - start) / num_nodes;
+        for (int i = 0; i < num_nodes; ++i) {
+            Mass *m;
+            if (i == num_nodes - 1)
+                m = new Mass(end, node_mass, false);
+            else
+                m = new Mass(start + step * i, node_mass, false);
+            masses.push_back(m);
+        }
 
-//        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+        // Comment-in this part when you implement the constructor
+        for (auto &i : pinned_nodes) {
+            masses[i]->pinned = true;
+        }
+
+        for (int i = 1; i < num_nodes; ++i) {
+            springs.push_back(new Spring(masses[i - 1], masses[i], k));
+        }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
     {
         for (auto &s : springs)
-        {
-            // TODO (Part 2): Use Hooke's law to calculate the force on a node
-        }
-
-        for (auto &m : masses)
-        {
-            if (!m->pinned)
             {
-                // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
-
-                // TODO (Part 2): Add global damping
+                // TODO (Part 2): Use Hooke's law to calculate the force on a node
             }
 
-            // Reset all forces on each mass
-            m->forces = Vector2D(0, 0);
-        }
+        for (auto &m : masses)
+            {
+                if (!m->pinned)
+                    {
+                        // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+
+                        // TODO (Part 2): Add global damping
+                    }
+
+                // Reset all forces on each mass
+                m->forces = Vector2D(0, 0);
+            }
     }
 
     void Rope::simulateVerlet(float delta_t, Vector2D gravity)
     {
         for (auto &s : springs)
-        {
-            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
-        }
+            {
+                // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            }
 
         for (auto &m : masses)
-        {
-            if (!m->pinned)
             {
-                Vector2D temp_position = m->position;
-                // TODO (Part 3.1): Set the new position of the rope mass
-                
-                // TODO (Part 4): Add global Verlet damping
+                if (!m->pinned)
+                    {
+                        Vector2D temp_position = m->position;
+                        // TODO (Part 3.1): Set the new position of the rope mass
+
+                        // TODO (Part 4): Add global Verlet damping
+                    }
             }
-        }
     }
 }
